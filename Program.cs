42e69daf43a3cf6f22389db4d32db11f0293f016
Kfir <kfirg15@gmail.com>
2020-04-02 17:41:36 +0300

    The main

--- Program.cs
+++ Program.cs
@@ -7,35 +7,80 @@ namespace Task_2
         //-------Main--------
         static void Main(string[] args)
         {
-            int choice;
-            Console.WriteLine("Hello Manager\n*\n*\n*");
-            Console.WriteLine("Animal Manager"); 
-            Console.WriteLine("\t1 - Add new Animal"); //1
-            Console.WriteLine("\t2 - Find animal by code");//2
-            Console.WriteLine("\t3 - Edit existing animal");//3
-            Console.WriteLine("\t4 - Print Animals By IsSea");//4
-            Console.WriteLine("\t5 - Print Animals Above 10kg and King is Female");//5
-            Console.WriteLine("\t6 - Exit");//6
-            choice = int.Parse(Console.ReadLine());
-            /*
-            Animal a1, a2;
-            a1 = addNewAnimal();
-            a1.printAnimalInfo();
-            a2 = addNewAnimal();
-            a2.printAnimalInfo();
-            Animal[] arr ={ a1, a2 };
-            PrintAnimalByIsSea(arr);*/
+            Animal[] animals = new Animal[100];
+            int choice = -1, idx = 0, tempCode;
+            Animal tempAnimal;
+            while (choice != 6)
+            {
+                ShowMenu();
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        animals[idx] = addNewAnimal();
+                        idx++;
+                        break;
+                    case 2:
+                        Console.WriteLine("Insert a code number:");
+                        tempCode = int.Parse(Console.ReadLine());
+                        tempAnimal = findAnimalByCode(animals, tempCode);
+                        if (tempAnimal == null)
+                        {
+                            Console.WriteLine("Didn't find");
+                            break;
+                        }
+                        tempAnimal.printAnimalInfo();
+                        break;
+                    case 3:
+                        Console.WriteLine("Insert a code number:");
+                        tempCode = int.Parse(Console.ReadLine());
+                        if(EditAnimal(animals,tempCode)==true)
+                        {
+                            tempAnimal = findAnimalByCode(animals, tempCode);
+                            tempAnimal=addNewAnimal();
+                        }
+                        break;
+                    case 4:
+                        PrintAnimalByIsSea(animals);
+                        break;
+                    case 5:
+                        PrintAnimalsAbove10AndFemale(animals);
+                        break;
+                    default:
+                        Console.WriteLine("Worng input");
+                        break;
+                }
+            }
+
         }
         public static Animal addNewAnimal()
-        {
-            Console.WriteLine("Insert animal information:code,name,kind,weight,is it water animal");
-            return new Animal(uint.Parse(Console.ReadLine()), Console.ReadLine(), char.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()), bool.Parse(Console.ReadLine()));
-        }
-        public static Animal findAnimalByCode( Animal [] arr, int code)
-        {
-            for(int i=0; i<arr.Length; i++)
+        { 
+            uint code; string name; float weight; char kind, t; bool isSea = false;
+            Console.WriteLine("\tAdd new animal:");
+            Console.WriteLine("Insert code number:");
+            code = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Insert name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Insert weight number:");
+            weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("The kind animal is Female?-press F\nThe kind animal is Male ? -prees M");
+            kind = char.Parse(Console.ReadLine());
+            Console.WriteLine("The animal live in water?-press Y\nother - press N");
+            t = char.Parse(Console.ReadLine());
+            if (t == 'Y')
+                isSea = true;
+            else
             {
-                if(arr[i].GetCode()==code)
+                if (t == 'N') isSea = false;
+                else Console.WriteLine("Worng input");
+            }
+            return new Animal(code, name, kind, weight, isSea);
+        }
+        public static Animal findAnimalByCode(Animal[] arr, int code)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].GetCode() == code)
                 {
                     arr[i].printAnimalInfo();
                     return arr[i];
@@ -43,10 +88,10 @@ namespace Task_2
             }
             return null; //the code dont match to any animal's code
         }
-        public static bool EditAnimal(Animal [] arr, int code)
+        public static bool EditAnimal(Animal[] arr, int code)
         {
             Animal temp = findAnimalByCode(arr, code);
-            if(temp ==null)
+            if (temp == null)
             {
                 Console.WriteLine("The animal does not exist");
                 return false;
@@ -58,17 +103,17 @@ namespace Task_2
             temp.SetIsSea(bool.Parse(Console.ReadLine()));
             return true;
         }
-        public static void PrintAnimalByIsSea(Animal [] arr)
+        public static void PrintAnimalByIsSea(Animal[] arr)
         {
-            string[] temp = new string[arr.Length+1]; // array of water animal
+            string[] temp = new string[arr.Length + 1]; // array of water animal
             int idxTemp = 0, j;
             bool check = true;
             Console.WriteLine("-------------------------------------------------------------\nThe water animals:");
-            for(int i=0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if(arr[i].GetIsSea() == true)
+                if (arr[i].GetIsSea() == true)
                 {
-                    for(j=0; i<=idxTemp; i++)
+                    for (j = 0; i <= idxTemp; i++)
                     {
                         if (arr[i].GetName() == temp[j])
                         {
@@ -78,8 +123,8 @@ namespace Task_2
                     }
                     if (check == true)
                     {
-                        Console.WriteLine("----------------------------");
                         arr[i].printAnimalInfo();
+                        Console.WriteLine("----------------------------");
                         temp[idxTemp] = arr[i].GetName();
                         idxTemp++;
                     }
@@ -87,9 +132,9 @@ namespace Task_2
                 check = true;
             }
         }
-        public static void PrintAnimalsAbove10AndFemale(Animal [] arr)
+        public static void PrintAnimalsAbove10AndFemale(Animal[] arr)
         {
-            for(int i=0; i<arr.Length;i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 if (arr[i].GetKind() == 'F' && arr[i].GetWeight() > 10.0)
                 {
@@ -97,6 +142,17 @@ namespace Task_2
                     arr[i].printAnimalInfo();
                 }
             }
+        }
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Hello Manager\n*\n*\n*");
+            Console.WriteLine("Animal Manager");
+            Console.WriteLine("\t1 - Add new Animal"); //1
+            Console.WriteLine("\t2 - Find animal by code");//2
+            Console.WriteLine("\t3 - Edit existing animal");//3
+            Console.WriteLine("\t4 - Print Animals By IsSea");//4
+            Console.WriteLine("\t5 - Print Animals Above 10kg and King is Female");//5
+            Console.WriteLine("\t6 - Exit");//6
         }
     }
 }
